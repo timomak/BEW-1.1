448a26ef2cb339d26b9d0dd5804929ce04ce178e
Timofey Makhlay <tmakhlay2@gmail.com>
2018-09-04 12:11:55 -0700

    Added MongoDB and Mongoose

--- rotten-potatoes-makeschool/app.js
+++ rotten-potatoes-makeschool/app.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes-makeschool', { useMongoClient: true});
 
+const Review = mongoose.model('Review', {
+  title: String
+});
 let reviews = [
   { title: "Great Review" },
   { title: "Next Review" }
 ]
 
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req,res) => {
-  res.render('reviews-index', {reviews: reviews});
+  Review.find()
+  .then(reveiws => {
+    res.render('reviews-index', {reviews: reviews});
+  })
+  .catch(err => {
+    console.log(err);
+  })
 })
 
 app.get('/reviews', (req, res) => {
