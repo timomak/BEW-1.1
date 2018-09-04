8a76826a52829713c75264e269616a0558a01d6e
Timofey Makhlay <tmakhlay2@gmail.com>
2018-09-04 12:42:59 -0700

    Added reviews form

--- rotten-potatoes-makeschool/app.js
+++ rotten-potatoes-makeschool/app.js
@@ -2,16 +2,20 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost/rotten-potatoes-makeschool', { useMongoClient: true});
 
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
+
 let reviews = [
   { title: "Great Review" },
   { title: "Next Review" }
 ]
-
+app.use(bodyParser.urlencoded({ extended: true}));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
@@ -25,8 +29,18 @@ app.get('/', (req,res) => {
   })
 })
 
-app.get('/reviews', (req, res) => {
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message)
+  })
+  // res.render('reviews-new', {})
+})
 
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
 })
 
 app.listen(3000, () => {
