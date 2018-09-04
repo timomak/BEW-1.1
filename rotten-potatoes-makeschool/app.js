ab1fe77cf0ce630c6f5ce12c3ca36e51e8faa065
Timofey Makhlay <tmakhlay2@gmail.com>
2018-09-04 11:03:47 -0700

    Add reviews route

--- rotten-potatoes-makeschool/app.js
+++ rotten-potatoes-makeschool/app.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req,res) => {
-  res.render('home', {msg : 'Hello World!'});
+  res.render('reviews-index', {reviews: reviews});
+})
+
+app.get('/reviews', (req, res) => {
+
 })
 
 app.listen(3000, () => {
