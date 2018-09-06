438dc6f549d145d4b6f5683b18593e4b58652c0d
Timofey Makhlay <tmakhlay2@gmail.com>
2018-09-06 11:32:45 -0700

    Updated Controller

--- rotten-potatoes-makeschool/app.js
+++ rotten-potatoes-makeschool/app.js
@@ -13,72 +13,10 @@ const Review = mongoose.model('Review', {
   movieTitle: String
 });
 
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
-
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true}));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-app.get('/', (req,res) => {
-  Review.find()
-  .then(reveiws => {
-    res.render('reviews-index', {reviews: reviews});
-  })
-  .catch(err => {
-    console.log(err);
-  })
-})
-
-app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect(`/reviews/${review._id}`);
-  }).catch((err) => {
-    console.log(err.message)
-  })
-  // res.render('reviews-new', {})
-})
-
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
-})
-
-app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id).then((review) => {
-      res.render('reviews-show', { review: review })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-
-app.get('/reviews/:id/edit', function (req, res) {
-  Review.findById(req.params.id, function(err, review) {
-    res.render('review-edit' , { review: review });
-  })
-})
-
-app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-app.delete('/reviews/:id', function(req, res) {
-  console.log("Delete review")
-  Review.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
 
 var routes = require('./controllers/reviews');
 routes(app, Review);
--- rotten-potatoes-makeschool/controllers/reviews.js
+++ rotten-potatoes-makeschool/controllers/reviews.js
@@ -1,13 +1,62 @@
 module.exports = function (app, Review) {
+  let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+  ]
 
-  app.get('/', (req, res) => {
+  app.get('/', (req,res) => {
     Review.find()
-      .then(reviews => {
-        res.render('reviews-index', {reviews: reviews});
+    .then(reveiws => {
+      res.render('reviews-index', {reviews: reviews});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  })
+  app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+      console.log(review);
+      res.redirect(`/reviews/${review._id}`);
+    }).catch((err) => {
+      console.log(err.message)
+    })
+    // res.render('reviews-new', {})
+  })
+
+  app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+  })
+
+  app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', { review: review })
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
+
+  app.get('/reviews/:id/edit', function (req, res) {
+    Review.findById(req.params.id, function(err, review) {
+      res.render('review-edit' , { review: review });
+    })
+  })
+
+  app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+      .then(review => {
+        res.redirect(`/reviews/${review._id}`)
       })
       .catch(err => {
-        console.log(err);
-      });
-  });
+        console.log(err.message)
+      })
+  })
 
+  app.delete('/reviews/:id', function(req, res) {
+    console.log("Delete review")
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  })
 }
